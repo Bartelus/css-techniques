a8e14f30fdc9f58c559bb760157e2ed05e206ab4
Bartelus <wilroed@gmail.com>
2024-09-06 12:11:34 +0200

    Modus kifte satt opp i javascript

---
+++ main.js
@@ -0,0 +1,27 @@
+function fargemodus() {
+    // logic for fargemodus
+    console.log("Du har trykket på modus knappen")
+
+    // Finn data-theme i HTML element
+let htmlKilde = document.querySelector("html")
+console.log (htmlKilde.dataset.theme)
+
+let aktivModus = htmlKilde.dataset.theme
+    // Les gjeldene verdi for data-theme
+
+
+    // Hvis gjeldende data-theme er "dark", then "light"
+    if (aktivModus === "dark"){
+htmlKilde.dataset.theme = "light"
+    } else {
+        
+        // else data-theme "dark"
+htmlKilde.dataset.theme = "dark"
+    }
+
+
+}
+
+// Find the button that should trigger the function
+let modusKnapp = document.getElementById("modus")
+modusKnapp.addEventListener("click", fargemodus)
