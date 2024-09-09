dfc1b92c68508c99b7c0fb904e2916528b6d442b
Bartelus <wilroed@gmail.com>
2024-09-09 16:20:24 +0200

    Navn og Alder felt

--- main.js
+++ main.js
@@ -25,3 +25,16 @@ htmlKilde.dataset.theme = "dark"
 // Find the button that should trigger the function
 let modusKnapp = document.getElementById("modus")
 modusKnapp.addEventListener("click", fargemodus)
+
+
+let nameCheck = document.getElementById("navn") 
+
+let ageCheck = document.getElementById("alder")
+let ageValue = ageCheck.value
+
+idCheck.addEventListener("click")
+
+function idkontroll() {
+}
+
+if (ageVaclue < 18) 
